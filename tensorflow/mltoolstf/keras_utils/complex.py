deb4c9ef3e5ab5aa9f9f2c691940d87d9db50931
Kerstin Hammernik <k.hammernik@imperial.ac.uk>
2020-10-09 17:05:27 +0100

    add CG

--- tensorflow/mltoolstf/keras_utils/complex.py
+++ tensorflow/mltoolstf/keras_utils/complex.py
@@ -9,6 +9,12 @@ def complex_norm(z, eps=1e-6):
 def complex_angle(z, eps=1e-6):
     return tf.math.atan2(tf.math.imag(z), tf.math.real(z) + eps)
 
+def complex_scale(x, scale):
+    return tf.complex(tf.math.real(x) * scale, tf.math.imag(x) * scale)
+    
+def complex_dot(x, y, axis=None):
+    return tf.reduce_sum(tf.math.conj(x) * y, axis=axis)
+
 def complex2real(z, channel_last=True):
     stack_dim = -1 if channel_last else 1
     return tf.concat([tf.math.real(z), tf.math.imag(z)], stack_dim)
@@ -17,6 +23,3 @@ def real2complex(z, channel_last=True):
     stack_dim = -1 if channel_last else 1
     (real, imag) = tf.split(z, 2, axis=stack_dim)
     return tf.complex(real, imag)
-
-def complex_scale(x, scale):
-    return tf.complex(tf.math.real(x) * scale, tf.math.imag(x) * scale)
