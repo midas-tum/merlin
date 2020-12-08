98a7298fe7761108ddc2ff92f2e683ca04b1f8c1
Kerstin Hammernik <k.hammernik@imperial.ac.uk>
2020-12-08 15:54:22 +0000

    save reconstructions.

--- python/mltools/utils.py
+++ python/mltools/utils.py
@@ -1,3 +1,6 @@
+import pathlib
+import h5py
+
 def center_crop(data, shape, channel_last=False):
     """
     [source] https://github.com/facebookresearch/fastMRI/blob/master/data/transforms.py
@@ -30,3 +33,22 @@ def center_crop(data, shape, channel_last=False):
         return data[..., w_from:w_to, h_from:h_to, :]
     else:
         return data[..., w_from:w_to, h_from:h_to]
+
+def save_reconstructions(reconstructions, out_dir):
+    """
+    [Code from https://github.com/facebookresearch/fastMRI]
+    Saves the reconstructions from a model into h5 files that is appropriate for submission
+    to the leaderboard.
+
+    Args:
+        reconstructions (dict[str, np.array]): A dictionary mapping input filenames to
+            corresponding reconstructions (of shape num_slices x height x width).
+        out_dir (pathlib.Path): Path to the output directory where the reconstructions
+            should be saved.
+    """
+    out_dir = pathlib.Path(out_dir).resolve()
+    out_dir.mkdir(parents=True, exist_ok=True)
+    for fname, recons in reconstructions.items():
+        fname = fname.split('/')[-1]
+        with h5py.File(out_dir / fname, 'w') as f:
+            f.create_dataset('reconstruction', data=recons)
