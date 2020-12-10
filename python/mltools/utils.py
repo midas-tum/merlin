0491bd037061ecc08eac8ef1a9401a7649db1936
Kerstin Hammernik <k.hammernik@imperial.ac.uk>
2020-12-10 15:35:54 +0000

    add random crop.

--- python/mltools/utils.py
+++ python/mltools/utils.py
@@ -1,5 +1,6 @@
 import pathlib
 import h5py
+import numpy as np
 
 def center_crop(data, shape, channel_last=False):
     """
@@ -26,6 +27,38 @@ def center_crop(data, shape, channel_last=False):
     assert 0 < shape[1] <= dim1
     w_from = (dim0 - shape[0]) // 2
     h_from = (dim1 - shape[1]) // 2
+    w_to = w_from + shape[0]
+    h_to = h_from + shape[1]
+
+    if channel_last:
+        return data[..., w_from:w_to, h_from:h_to, :]
+    else:
+        return data[..., w_from:w_to, h_from:h_to]
+
+def random_crop(data, shape, channel_last=False):
+    """
+    Apply a random crop to the input real image or batch of real images.
+
+    Args:
+        data (numpy.array): The input tensor to be center cropped. It should have at
+            least 2 dimensions and the cropping is applied along the last two dimensions.
+        shape (int, int): The output shape. The shape should be smaller than the
+            corresponding dimensions of data.
+
+    Returns:
+        numpy.array: The center cropped image
+    """
+    if channel_last:
+        dim0 = data.shape[-3]
+        dim1 = data.shape[-2]
+    else:
+        dim0 = data.shape[-2]
+        dim1 = data.shape[-1]
+
+    assert 0 < shape[0] <= dim0
+    assert 0 < shape[1] <= dim1
+    w_from = np.random.randint(0, dim0 - shape[0])
+    h_from = np.random.randint(0, dim1 - shape[1])
     w_to = w_from + shape[0]
     h_to = h_from + shape[1]
 
