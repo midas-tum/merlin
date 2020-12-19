8b225908c7a82a3020f640a728c1c842979ff6bc
Thomas <thomas.kuestner@uni-tuebingen.de>
2020-12-19 13:47:46 +0100

    add parser arguments

--- python/merlinpy/experiment.py
+++ python/merlinpy/experiment.py
@@ -34,11 +34,17 @@ class Experiment(object):
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument('--config', type=pathlib.Path, default='./config.yml', help='Path to the config file')
         self.parser.add_argument('--experiment', type=str, required=True, help='Experiment name')
+        self.parser.add_argument('-t', '--task', type=int, default=0, help='Task ID to be performed')
         self.parser.add_argument('--gpu', type=int, default=0, help='To be used GPU')
+        action = self.parser.add_mutually_exclusive_group(required=False)
+        action.add_argument('--tf', action='store_true', default=True, help='Use Tensorflow backend (default)'))
+        action.add_argument('--th', action='store_true', default=False, help='Use Pytorch backend'))
         self.parser.add_argument('--resume', action='store_true', default=False, help='Resume training')
-        self.parser.add_argument('--train', action='store_true', default=True, help='Train the model')
-        self.parser.add_argument('--evaluate', action='store_true', default=False, help='Evaluate the model')
-        self.parser.add_argument('--predict', action='store_true', default=False, help='Predict the model')
+        action = self.parser.add_mutually_exclusive_group(required=False)
+        action.add_argument('--train', action='store_true', default=True, help='Train the model')
+        action.add_argument('--evaluate', action='store_true', default=False, help='Evaluate the model')
+        action.add_argument('--predict', action='store_true', default=False, help='Predict the model')
+        action.add_argument('--mode', type=str, default='train', help='Processing mode: ''train'' | ''evaluate'' | ''predict'' | ''zerofilling'' | ''cgsense''')
 
         self.config = config
 
