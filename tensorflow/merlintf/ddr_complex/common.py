d4d4e54b5dfc83180dba494220e79f5f2572e259
Kerstin Hammernik <k.hammernik@imperial.ac.uk>
2021-01-20 11:37:20 +0000

    weight generation in build.

--- tensorflow/merlintf/ddr_complex/common.py
+++ tensorflow/merlintf/ddr_complex/common.py
@@ -2,17 +2,30 @@ import tensorflow as tf
 import unittest
 import merlintf.keras_utils
 import numpy as np
-import six
 
 __all__ = ['Scalar']
 
 class Scalar(tf.keras.layers.Layer):
-    def __init__(self, name, constraint=None, initializer=None):
+    def __init__(self, name, scale=1, trainable=True,
+                    constraint=None, initializer=None):
         super().__init__(name=name)
-        self.weight = self.add_weight(shape=(1,),
-                name=name,
-                constraint=constraint,
-                initializer=initializer)
+
+        self.scale = scale # to potentially accelerate training!
+        self.weight_constraint = constraint
+        self.weight_initializer = initializer
+        self.weight_trainable = trainable
+        self.weight_name = name
+
+    def build(self, input_shape):
+        self._weight = self.add_weight(shape=(1,),
+                name=self.weight_name,
+                constraint=self.weight_constraint,
+                initializer=self.weight_initializer,
+                trainable=self.weight_trainable)
+
+    @property
+    def weight(self):
+        return self._weight * self.scale
         
     def call(self, inputs):
         if merlintf.keras_utils.iscomplextf(inputs):
