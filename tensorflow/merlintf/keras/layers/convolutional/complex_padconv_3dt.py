07737c367d241d17fae859f66577b62584de194a
Kerstin Hammernik <k.hammernik@imperial.ac.uk>
2021-02-02 10:26:20 +0000

    fix complex padconv 3dt.

--- tensorflow/merlintf/keras/layers/convolutional/complex_padconv_3dt.py
+++ tensorflow/merlintf/keras/layers/convolutional/complex_padconv_3dt.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.python.keras import constraints
 from tensorflow.python.keras import initializers
 from tensorflow.python.keras import regularizers
+from tensorflow.python.keras.utils import conv_utils
 
 from merlintf.keras.layers.convolutional.complex_padconv import ComplexPadConv3D, ComplexPadConvScale3D
 
@@ -33,6 +34,7 @@ class ComplexPadConv3Dt(tf.keras.layers.Layer):
         super(ComplexPadConv3Dt, self).__init__()
 
         self.intermediate_filters = intermediate_filters
+        self.data_format = conv_utils.normalize_data_format(data_format)
 
         if strides[1] > 2:
             conv_module = ComplexPadConvScale3D
@@ -81,33 +83,69 @@ class ComplexPadConv3Dt(tf.keras.layers.Layer):
         pad=pad,
         **kwargs)
 
-    def call(self, x):
-        if self._data_format == 'channels_first':
-            x_sp = tf.stack([self.conv_xyz(tf.squeeze(x[:, :, i, :, :, :], axis=[2])) for i in range(0, self.input_shape[2])], axis=2)
-            x_t = tf.stack([self.conv_t(tf.squeeze(x_sp[:, :, :, :, :, i], axis=[5])) for i in range(0, self.input_shape[5])], axis=5)
+    def build(self, input_shape):
+        self.shape = input_shape
+        input_shape = list(input_shape)
+        shape_xyz = input_shape.copy()
+        shape_t = input_shape.copy()
+
+        if self.data_format == 'channels_first':
+            shape_xyz.pop(2)
+            shape_t.pop(5)
+            shape_t[1] = self.intermediate_filters
         else:
-            x_sp = tf.stack([self.conv_xyz(tf.squeeze(x[:, i, :, :, :, :], axis=[1])) for i in range(0, self.input_shape[1])], axis=1)
-            x_t = tf.stack([self.conv_t(tf.squeeze(x_sp[:, :, :, :, i, :], axis=[4])) for i in range(0, self.input_shape[4])],  axis=4)
+            shape_xyz.pop(1)
+            shape_t.pop(4)
+            shape_t[-1] = self.intermediate_filters
+
+        self.conv_xyz.build(shape_xyz)
+        self.conv_t.build(shape_t)
+
+    def call(self, x):
+        if self.data_format == 'channels_first':
+            x_sp = tf.stack([self.conv_xyz(x[:, :, i, :, :, :]) for i in range(0, self.shape[2])], axis=2)
+            x_t = tf.stack([self.conv_t(x_sp[:, :, :, :, :, i]) for i in range(0, self.shape[5])], axis=5)
+        else:
+            x_sp = tf.stack([self.conv_xyz(x[:, i, :, :, :, :]) for i in range(0, self.shape[1])], axis=1)
+            x_t = tf.stack([self.conv_t(x_sp[:, :, :, :, i, :]) for i in range(0, self.shape[4])], axis=4)
         return x_t
 
     def backward(self, x, output_shape=None):
-        if self._data_format == 'channels_first':
-            xT_t = tf.stack([self.conv_t.backward(tf.squeeze(x[:, :, :, :, :, i], axis=[5]), output_shape) for i in range(0, self.input_shape[5])], axis=5)
-            xT_sp = tf.stack([self.conv_xyz.backward(tf.squeeze(xT_t[:, :, i, :, :, :], axis=[2]), output_shape) for i in range(0, self.input_shape[2])], axis=2)
+        if output_shape is None:
+            shape_xyz = None
+            shape_t = None
         else:
-            xT_t = tf.stack([self.conv_t.backward(tf.squeeze(x[:, :, :, :, i, :], axis=[4]), output_shape) for i in range(0, self.input_shape[4])], axis=4)
-            xT_sp = tf.stack([self.conv_xyz.backward(tf.squeeze(xT_t[:, i, :, :, :, :], axis=[1]), output_shape) for i in range(0, self.input_shape[1])], axis=1)
+            output_shape = list(output_shape)
+            shape_xyz = output_shape.copy()
+            shape_t = output_shape.copy()
+
+            if self.data_format == 'channels_first':
+                shape_xyz.pop(2)
+                shape_t.pop(5)
+                shape_xyz[1] = self.intermediate_filters
+            else:
+                shape_xyz.pop(1)
+                shape_t.pop(4)
+                shape_xyz[-1] = self.intermediate_filters
+                
+
+        if self.data_format == 'channels_first':
+            xT_t = tf.stack([self.conv_t.backward(x[:, :, :, :, :, i], shape_t) for i in range(0, self.shape[5])], axis=5)
+            xT_sp = tf.stack([self.conv_xyz.backward(xT_t[:, :, i, :, :, :], shape_xyz) for i in range(0, self.shape[2])], axis=2)
+        else:
+            xT_t = tf.stack([self.conv_t.backward(x[:, :, :, :, i, :], shape_t) for i in range(0, self.shape[4])], axis=4)
+            xT_sp = tf.stack([self.conv_xyz.backward(xT_t[:, i, :, :, :, :], shape_xyz) for i in range(0, self.shape[1])], axis=1)
         return xT_sp
 
 class ComplexPadConv3dtTest(unittest.TestCase):
     def test_grad(self):
-        nBatch = 5
-        M = 128
-        N = 128
-        D = 24
-        T = 16
+        nBatch = 2
+        M = 32
+        N = 32
+        D = 12
+        T = 8
         nf_in = 2
-        nf_out = 32
+        nf_out = 16
         shape = [nBatch, T, M, N, D, nf_in]
         ksz = (3,5,5,5)
         nf_inter = np.ceil((nf_out * nf_in * np.prod(ksz)) / (nf_in * ksz[1] * ksz[2] * ksz[3] + nf_out * ksz[0])).astype(np.int32)
@@ -128,13 +166,13 @@ class ComplexPadConv3dtTest(unittest.TestCase):
         self.assertTrue(np.sum(np.abs(x_autograd - x_bwd))/x_autograd.size < 1e-5)
 
     def test_adjoint(self):
-        nBatch = 5
-        M = 128
-        N = 128
-        D = 24
-        T = 16
+        nBatch = 2
+        M = 64
+        N = 64
+        D = 12
+        T = 8
         nf_in = 2
-        nf_out = 32
+        nf_out = 16
         shape = [nBatch, T, M, N, D, nf_in]
         ksz = (3,5,5,5)
         nf_inter = np.ceil((nf_out * nf_in * np.prod(ksz)) / (nf_in * ksz[1] * ksz[2] * ksz[3] + nf_out * ksz[0])).astype(np.int32)
