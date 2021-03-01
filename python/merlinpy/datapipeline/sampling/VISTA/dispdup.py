ce3cdf5dd93de4528527be4cf085a0057d19e39f
SiyingXu <73036875+SiyingXu@users.noreply.github.com>
2021-03-01 21:30:13 +0100

    Update dispdup.py

--- python/merlinpy/datapipeline/sampling/VISTA/dispdup.py
+++ python/merlinpy/datapipeline/sampling/VISTA/dispdup.py
@@ -13,25 +13,30 @@ def ind2xy(ind, X):
         y.append(math.ceil(every_ind/X))
     x = np.array(x)
     y = np.array(y)
-    return x,y
+    return x, y
+
+def ind2xy_single(ind, X):
+    x = ind - math.floor((ind-1)/X)*X
+    y = math.ceil(ind/X)
+    return x, y
 
 #For a given 'dupind', this function finds the nearest vacant location among 'empind'
 def nearestvac(dupind, empind, p):
-  x0, y0 = ind2xy(dupind, p)
-  x, y = ind2xy(empind, p)
-  dis1 = np.array(list(map(square, (x-x0))))
-  dis2 = np.array(list(map(square, (y-y0))))
-  eps = np.spacing(1)
-  for dis2_ind in range(len(dis2)):
-    if dis2[dis2_ind] > eps:
-      dis2[dis2_ind] = np.Inf
-  dis = []
-  for dis12 in (dis1+dis2):
-    dis_ = math.sqrt(dis12)
-    dis.append(dis_)
-  b = dis.index(min(dis))
-  n = empind[b]
-  return n
+    x0, y0 = ind2xy_single(dupind, p)
+    x, y = ind2xy(empind, p)
+    dis1 = np.array(list(map(square, (x-x0)))).astype(float)
+    dis2 = np.array(list(map(square, (y-y0)))).astype(float)
+    eps = np.spacing(1)
+    for dis2_ind in range(len(dis2)):
+        if dis2[dis2_ind] > eps:
+            dis2[dis2_ind] = np.Inf
+    dis = []
+    for dis12 in (dis1+dis2):
+        dis_ = math.sqrt(dis12)
+        dis.append(dis_)
+    b = dis.index(min(dis))
+    n = empind[b]
+    return n
 
 def dispdup(ph, ti, p, t):
     """
@@ -42,8 +47,6 @@ def dispdup(ph, ti, p, t):
     p: Number of phase encoding steps
     t: Number of frames
     """
-    ph = ph.flatten()
-    ti = ti.flatten()
     ph = ph + math.ceil((p+1)/2)
     ti = ti + math.ceil((t+1)/2)
     pt = (ti-1)*p + ph
@@ -62,7 +65,7 @@ def dispdup(ph, ti, p, t):
     empind = np.setdiff1d(np.array([x for x in range(0, p * t)]), pt)
 
     for i in range(len(dupind)):  # Let's go through all 'dupind' one by one
-        newind = nearestvac(list(pt[dupind[i]]), empind, p)
+        newind = nearestvac(pt[dupind[i]], empind, p)
         pt[dupind[i]] = newind
         empind = np.setdiff1d(empind, np.array(newind))
 
