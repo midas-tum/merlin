c1dd089347f13055ba1493d1ba9eb6a6b6a3ded5
KaijieMo1 <69183027+KaijieMo1@users.noreply.github.com>
2021-07-19 13:32:43 +0200

    Update complex_pool.py

--- tensorflow/merlintf/keras/layers/complex_pool.py
+++ tensorflow/merlintf/keras/layers/complex_pool.py
@@ -32,62 +32,84 @@ def deserialize(op):
         raise ValueError(f"Selected operation '{op}' not implemented in complex convolutional")
 
 class MagnitudeMaxPool(tf.keras.layers.Layer):
-    def __init__(self, pool_size, strides=None, padding='SAME', optox=True):
+    def __init__(self, pool_size, strides=None, padding='SAME', optox=True,argmax_index=False):
         super(MagnitudeMaxPool, self).__init__()
         self.pool_size = pool_size
         if strides is None:
             strides = pool_size
         self.strides = strides
-        self.padding =  padding
-        self.alpha = 1 # magnitude ratio in real part
+        self.padding = padding
+        self.alpha = 1  # magnitude ratio in real part
         self.beta = 1  # magnitude ratio in imag part
         self.optox = optox  # True: execute Optox pooling; False: use TF pooling (not supported for all cases)
+        self.argmax_index=argmax_index
 
     def call(self, x):  # default to TF
         xabs = merlintf.complex_abs(x)
         _, idx = tf.nn.max_pool_with_argmax(
-    xabs, self.pool_size, self.strides, self.padding, include_batch_in_index=True)
-        x_pool = tf.reshape(tf.gather(tf.reshape(x,shape= [-1,]),idx), shape=idx.shape)
+            xabs, self.pool_size, self.strides, self.padding, include_batch_in_index=True)
+        x_pool = tf.reshape(tf.gather(tf.reshape(x, shape=[-1, ]), idx), shape=idx.shape)
 
         return x_pool
+
 
 class MagnitudeMaxPool1D(MagnitudeMaxPool):
     def __init__(self, pool_size, strides=None, padding='SAME', optox=True):
         super(MagnitudeMaxPool1D, self).__init__(pool_size, strides, padding, optox)
 
+
 class MagnitudeMaxPool2D(MagnitudeMaxPool):
-    def __init__(self, pool_size, strides=None, padding='SAME', optox=True):
-        super(MagnitudeMaxPool2D, self).__init__(pool_size, strides, padding, optox)
-
-    def call(self,x ):
-        if self.optox:
-            if merlintf.iscomplextf(x):
-                x_pool, _ = optotf.maxpooling.maxpooling2d(x, pooling=self.pool_size, stride=self.strides,
-                                                           alpha=self.alpha, beta=self.beta, mode=self.padding)
-            else:
-                x_pool = tf.nn.max_pool(x, ksize=self.pool_size, strides=self.strides, padding=self.padding)
-            return x_pool
-        else:
-            return super().call(x)
-
-class MagnitudeMaxPool3D(MagnitudeMaxPool):
-    def __init__(self, pool_size, strides=None, padding='SAME', optox=True):
-        super(MagnitudeMaxPool3D, self).__init__(pool_size, strides, padding, optox)
+    def __init__(self, pool_size, strides=None, padding='SAME', optox=True,argmax_index=False):
+        super(MagnitudeMaxPool2D, self).__init__(pool_size, strides, padding, optox,argmax_index)
 
     def call(self, x):
         if self.optox:
             if merlintf.iscomplextf(x):
-                x_pool, _ = optotf.maxpooling.maxpooling3d(x, pooling=self.pool_size, stride=self.strides,
+
+                x_pool, x_pool_index = optotf.maxpooling.maxpooling2d(x, pooling=self.pool_size, stride=self.strides,
                                                            alpha=self.alpha, beta=self.beta, mode=self.padding)
+                if self.argmax_index:
+                    return x_pool, x_pool_index
+                else:
+                    return x_pool
             else:
-                x_pool = tf.nn.max_pool3d(x, ksize=self.pool_size, strides=self.strides, padding=self.padding)
-            return x_pool
+                x_pool, x_pool_index  = tf.nn.max_pool_with_argmax(x, ksize=self.pool_size, strides=self.strides, padding=self.padding)
+                if self.argmax_index:
+                    return x_pool, x_pool_index
+                else:
+                    return x_pool
         else:
             return super().call(x)
 
+
+class MagnitudeMaxPool3D(MagnitudeMaxPool):
+    def __init__(self, pool_size, strides=None, padding='SAME', optox=True,argmax_index=False):
+        super(MagnitudeMaxPool3D, self).__init__(pool_size, strides, padding, optox,argmax_index)
+
+    def call(self, x):
+        if self.optox:
+            if merlintf.iscomplextf(x):
+                x_pool, x_pool_index = optotf.maxpooling.maxpooling3d(x, pooling=self.pool_size, stride=self.strides,
+                                                           alpha=self.alpha, beta=self.beta, mode=self.padding)
+
+                if self.argmax_index:
+                    return x_pool, x_pool_index
+                else:
+                    return x_pool
+            else:
+                if self.argmax_index:
+                    x_pool, x_pool_index  = tf.nn.max_pool3d(x, ksize=self.pool_size, strides=self.strides, padding=self.padding)
+                    return x_pool, x_pool_index
+                else:
+                    x_pool=tf.nn.max_pool3d(x, ksize=self.pool_size, strides=self.strides, padding=self.padding)
+                    return x_pool
+        else:
+            return super().call(x)
+
+
 class MagnitudeMaxPool2Dt(MagnitudeMaxPool):
-    def __init__(self, pool_size, strides=None, padding='SAME', optox=True):
-        super(MagnitudeMaxPool2Dt, self).__init__(pool_size, strides, padding, optox)
+    def __init__(self, pool_size, strides=None, padding='SAME', optox=True,argmax_index=False):
+        super(MagnitudeMaxPool2Dt, self).__init__(pool_size, strides, padding, optox,argmax_index)
 
     def call(self, x):
         if self.optox:
@@ -97,50 +119,64 @@ class MagnitudeMaxPool2Dt(MagnitudeMaxPool):
             x = tf.reshape(x, batched_shape)
 
             if merlintf.iscomplextf(x):
-                x_pool, _ = optotf.maxpooling.maxpooling2d(x, pooling=self.pool_size, stride=self.strides,
+                x_pool, x_pool_index = optotf.maxpooling.maxpooling2d(x, pooling=self.pool_size, stride=self.strides,
                                                            alpha=self.alpha, beta=self.beta, mode=self.padding)
+
             else:
-                x_pool = tf.nn.max_pool(x, ksize=self.pool_size, strides=self.strides, padding=self.padding)
+                x_pool,x_pool_index  = tf.nn.max_pool_with_argmax(x, ksize=self.pool_size, strides=self.strides, padding=self.padding)
 
             pooled_shape = [orig_shape[0], orig_shape[1], x_pool.shape[1], x_pool.shape[2], orig_shape[-1]]
             x_pool = tf.reshape(x_pool, pooled_shape)
-            return x_pool
+            if self.argmax_index:
+                return x_pool, x_pool_index
+            else:
+                return x_pool
         else:
             orig_shape = x.shape
             rank = tf.rank(x)
-            batched_shape = [x.shape[0]*x.shape[1], x.shape[2], x.shape[3], x.shape[4]]
+            batched_shape = [x.shape[0] * x.shape[1], x.shape[2], x.shape[3], x.shape[4]]
             x = tf.reshape(x, batched_shape)
 
             xabs = merlintf.complex_abs(x)
             _, idx = tf.nn.max_pool_with_argmax(
-        xabs, self.pool_size, self.strides, self.padding, include_batch_in_index=True)
-            x_pool = tf.reshape(tf.gather(tf.reshape(x,shape= [-1,]),idx), shape=idx.shape)
+                xabs, self.pool_size, self.strides, self.padding, include_batch_in_index=True)
+            x_pool = tf.reshape(tf.gather(tf.reshape(x, shape=[-1, ]), idx), shape=idx.shape)
 
             pooled_shape = [orig_shape[0], orig_shape[1], x_pool.shape[1], x_pool.shape[2], orig_shape[-1]]
             x_pool = tf.reshape(x_pool, pooled_shape)
-            return x_pool
+            if self.argmax_index:
+                return x_pool,idx
+            else:
+                return x_pool
+
 
 class MagnitudeMaxPool3Dt(MagnitudeMaxPool):
-    def __init__(self, pool_size, strides=None, padding='SAME', optox=True):
-        super(MagnitudeMaxPool3Dt, self).__init__(pool_size, strides, padding, optox)
+    def __init__(self, pool_size, strides=None, padding='SAME', optox=True,argmax_index=False):
+        super(MagnitudeMaxPool3Dt, self).__init__(pool_size, strides, padding, optox,argmax_index)
 
     def call(self, x):  # only Optox supported
         orig_shape = x.shape
         rank = tf.rank(x)
-        if rank==6:
-            batched_shape = [x.shape[0]*x.shape[1], x.shape[2], x.shape[3], x.shape[4], x.shape[5]]
+        if rank == 6:
+            batched_shape = [x.shape[0] * x.shape[1], x.shape[2], x.shape[3], x.shape[4], x.shape[5]]
         elif rank == 5:
             batched_shape = [x.shape[0] * x.shape[1], x.shape[2], x.shape[3], x.shape[4]]
 
         if merlintf.iscomplextf(x):
-            x_pool = optotf.maxpooling.maxpooling3d(x, pooling=self.pool_size, stride=self.strides, alpha=self.alpha, beta=self.beta, mode=self.padding)
+            x_pool, x_pool_index= optotf.maxpooling.maxpooling3d(x, pooling=self.pool_size, stride=self.strides, alpha=self.alpha,
+                                                    beta=self.beta, mode=self.padding)
         else:
-            x_pool = tf.nn.max_pool(x, ksize=self.pool_size, strides=self.strides, padding=self.padding)
+            # same as above
+            x_pool, x_pool_index= optotf.maxpooling.maxpooling3d(x, pooling=self.pool_size, stride=self.strides, alpha=self.alpha,
+                                                    beta=self.beta, mode=self.padding)
 
         pooled_shape = [orig_shape[0], orig_shape[1], x_pool.shape[1], x_pool.shape[2], orig_shape[-1]]
         x_pool = tf.reshape(x_pool, pooled_shape)
-        return x_pool
 
+        if self.argmax_index:
+            return x_pool, idx
+        else:
+            return x_pool
 
 class TestMagnitudePool(unittest.TestCase):
     def _test(self, shape, pool_size=2, strides=2):
