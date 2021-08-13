02156b891502ba11cc2611b327082123688ec3b7
Kerstin Hammernik <k.hammernik@tum.de>
2021-08-13 12:15:14 +0200

    #24 fix includes, run some tests.

--- python/merlinpy/datapipeline/sampling/VISTA/vista_main.py
+++ python/merlinpy/datapipeline/sampling/VISTA/vista_main.py
@@ -1,21 +1,29 @@
 import numpy as np
 from tqdm import tqdm
-from ..sampling import VISTA
+from merlinpy.datapipeline.sampling import VISTA
 
+#import medutils
 
 def main():
-    p_list = [132, 144, 150, 156, 162, 174, 180, 186, 192]
+    #p_list = [132, 144, 150, 156, 162, 174, 180, 186, 192]
+    p_list = [150]
+    mode = 'VISTA'
+    #R_list = range(2, 25)
+    R_list = [8, 16, 24]
+    frames = 25
+    sd_list = range(10, 201, 10)
+    sd_list = [42]
+
     for p in tqdm(p_list):
         print('nFE:', p)
-        dim = np.array((1, p, 1, 25, 1))
-
-        for R in tqdm(range(2, 25)):
+        dim = np.array((1, p, 1, frames, 1))
+        for R in tqdm(R_list):
             print('acceleration factor is:', R)
-            for sd in tqdm(range(10, 201, 10)):
+            for sd in tqdm(sd_list):
                 print('mask: %d' % (sd/10))
-                mask = VISTA(dim, R, 'VISTA', sd=sd)
-                mask_VISTA = mask.generate_mask()
-                np.savetxt("mask_VISTA_%dx%d_acc%d_%d.txt" % (p, 25, R, sd/10), mask_VISTA, fmt="%d", delimiter=",")
+                mask = VISTA(dim, R, mode, sd=sd).generate_mask()
+                np.savetxt("mask_%s_%dx%d_acc%d_%d.txt" % (mode, p, frames, R, sd/10), mask, fmt="%d", delimiter=",")
+                #medutils.visualization.imsave(mask, f'mask_{mode}_pe{p}_frames{frames}_acc{R}_sd{sd}.png')
 
 
 if __name__ == '__main__':
