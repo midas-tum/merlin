560e8995c6b02a689e95f9bd8f801fc749a30af8
Thomas <thomas.kuestner@uni-tuebingen.de>
2021-09-15 18:33:28 +0200

    include .so modules

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
+from setuptools.dist import Distribution
 import os
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
 
 setup(
     name='merlinpy',
-    version='0.2.0',
+    version='0.2.1',
     author="Kerstin Hammernik and Thomas Kuestner",
     author_email="k.hammernik@imperial.ac.uk and thomas.kuestner@med.uni-tuebingen.de",
     packages=["merlinpy",
@@ -11,7 +16,7 @@ setup(
               "merlinpy.wandb",
               "merlinpy.datapipeline",
               "merlinpy.datapipeline.sampling",
-              "merlinpy.datapipeline.sampling.VISTA"
+              "merlinpy.datapipeline.sampling.VISTA",
               ],
     package_dir={"merlinpy": os.path.join('.', "merlinpy"),
                  "merlinpy.fastmri": os.path.join('.', "merlinpy/fastmri"),
@@ -20,6 +25,9 @@ setup(
                  "merlinpy.datapipeline.sampling": os.path.join('.', "merlinpy/datapipeline/sampling"),
                  "merlinpy.datapipeline.sampling.VISTA": os.path.join('.', "merlinpy/datapipeline/sampling/VISTA"),
     },
+    include_package_data=True,
+    package_data={"merlinpy.datapipeline.sampling": ['*.so'],
+    },
     install_requires=[
         "numpy >= 1.15",
         "xmltodict",
@@ -27,4 +35,5 @@ setup(
         "pandas",
         "tqdm"
     ],
+    distclass=BinaryDistribution,
 )
