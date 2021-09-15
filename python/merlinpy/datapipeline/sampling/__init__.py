1d4624952df9ae172f843fccef63d7cb8df31fb1
Thomas <thomas.kuestner@uni-tuebingen.de>
2021-09-15 18:35:03 +0200

    add Poisson Disc and Gaussian sampling

--- python/merlinpy/datapipeline/sampling/__init__.py
+++ python/merlinpy/datapipeline/sampling/__init__.py
@@ -1,4 +1,4 @@
 from merlinpy.datapipeline.sampling.sampling import VISTA
 from merlinpy.datapipeline.sampling.sampling import Gaussian
 from merlinpy.datapipeline.sampling.sampling import CASPR
-from merlinpy.datapipeline.sampling.sampling import VDPD
+from merlinpy.datapipeline.sampling.sampling import PoissonDisc
