295ff8353f2f694623936cd4308f6c361882c2cc
Thomas <thomas.kuestner@uni-tuebingen.de>
2021-09-21 12:12:37 +0200

    split sampling codes

--- python/merlinpy/datapipeline/sampling/__init__.py
+++ python/merlinpy/datapipeline/sampling/__init__.py
@@ -1,4 +1,6 @@
 from merlinpy.datapipeline.sampling.sampling import VISTA
+from merlinpy.datapipeline.sampling.sampling import UIS
+from merlinpy.datapipeline.sampling.sampling import VRS
 from merlinpy.datapipeline.sampling.sampling import Gaussian
 from merlinpy.datapipeline.sampling.sampling import CASPR
 from merlinpy.datapipeline.sampling.sampling import PoissonDisc
