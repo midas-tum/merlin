8d1539e3b31cffb8e7f05b10b55fd2b4d54ac31a
Kerstin Hammernik <k.hammernik@tum.de>
2021-11-15 13:45:57 +0100

    #23 ddo not load padconv if optox is not available.

--- pytorch/merlinth/__init__.py
+++ pytorch/merlinth/__init__.py
@@ -2,10 +2,7 @@ from .utils import *
 from .complex import *
 from . import optim
 from . import layers
-# try:
-#     from . import layers
-# except:
-#     print('layers could not be loaded. Optox might not be installed.')
+
 try:
     from . import models
 except:
--- pytorch/merlinth/layers/__init__.py
+++ pytorch/merlinth/layers/__init__.py
@@ -1,23 +1,27 @@
 from merlinth.layers.convolutional.complex_conv import ComplexConv1d
-from merlinth.layers.convolutional.padconv import PadConv1d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConv1d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConvRealWeight1d
 from merlinth.layers.convolutional.complex_conv import ComplexConv2d
-from merlinth.layers.convolutional.padconv import PadConv2d
-from merlinth.layers.convolutional.padconv import PadConvScale2d
-from merlinth.layers.convolutional.padconv import PadConvScaleTranspose2d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConv2d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConvScale2d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConvScaleTranspose2d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConvRealWeight2d
 from merlinth.layers.convolutional.complex_conv import ComplexConv3d
-from merlinth.layers.convolutional.padconv import PadConv3d
-from merlinth.layers.convolutional.padconv import PadConvScale3d
-from merlinth.layers.convolutional.padconv import PadConvScaleTranspose3d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConv3d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConvScale3d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConvScaleTranspose3d
-from merlinth.layers.convolutional.complex_padconv import ComplexPadConvRealWeight3d
+
+try:
+    from merlinth.layers.convolutional.padconv import PadConv1d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConv1d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConvRealWeight1d
+    from merlinth.layers.convolutional.padconv import PadConv2d
+    from merlinth.layers.convolutional.padconv import PadConvScale2d
+    from merlinth.layers.convolutional.padconv import PadConvScaleTranspose2d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConv2d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConvScale2d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConvScaleTranspose2d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConvRealWeight2d
+    from merlinth.layers.convolutional.padconv import PadConv3d
+    from merlinth.layers.convolutional.padconv import PadConvScale3d
+    from merlinth.layers.convolutional.padconv import PadConvScaleTranspose3d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConv3d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConvScale3d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConvScaleTranspose3d
+    from merlinth.layers.convolutional.complex_padconv import ComplexPadConvRealWeight3d
+except:
+    print('padconv layers could not be loaded. Optox might not be installed.')
 # from merlinth.layers.convolutional.complex_conv3d import ComplexPadConv2Dt
 # from merlinth.layers.pad import real_pad2d
 # from merlinth.layers.pad import real_pad2d_transpose
