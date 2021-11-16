a61e3b80da0467d6ddf2662c2cff4eb1d91f763a
Kerstin Hammernik <k.hammernik@tum.de>
2021-11-16 15:23:28 +0100

    #23 remove torch utils. add dc layers, mri, fft  ops to merlinth. also addresses #2

---
+++ pytorch/merlinth/layers/data_consistency.py
@@ -0,0 +1,116 @@
+import torch
+import unittest
+from merlinth.layers.complex_cg import CGClass
+import unittest
+import numpy as np
+import merlinth
+
+class DCGD(torch.nn.Module):
+    def __init__(self, A, AH, weight_init=1.0, weight_scale=1.0, requires_grad=True, **kwargs):
+        super().__init__()
+
+        self.A = A
+        self.AH = AH
+
+        self.weight_scale = weight_scale
+        self.weight_init = weight_init
+        self._weight = torch.nn.Parameter(torch.tensor(1, dtype=torch.float32)*weight_init)
+        self._weight.requires_grad_(requires_grad)
+        self._weight.proj = lambda: self._weight.data.clamp_(1e-4, 1000)
+
+    @property
+    def weight(self):
+        return self._weight * self.weight_scale
+
+    def forward(self, inputs, scale=1.0):
+        x = inputs[0]
+        y = inputs[1]
+        constants = inputs[2:]
+        stepsize = self.weight * scale
+        return x - stepsize * self.AH(self.A(x, *constants) - y, *constants)
+
+    def __repr__(self):
+        return f'DCGD(lambda_init={self.weight_init:.4g}, weight_scale={self.weight_scale})'
+
+class DCPM(torch.nn.Module):
+    def __init__(self, A, AH, weight_init=1.0, weight_scale=1.0, requires_grad=True, **kwargs):
+        super().__init__()
+
+        self.A = A
+        self.AH = AH
+
+        self.weight_scale = weight_scale
+        self.weight_init = weight_init
+        self._weight = torch.nn.Parameter(torch.tensor(1, dtype=torch.float32)*weight_init)
+        self._weight.requires_grad_(requires_grad)
+        self._weight.proj = lambda: self._weight.data.clamp_(1e-4, 1000)
+
+        max_iter = kwargs.get('max_iter', 10)
+        tol = kwargs.get('tol', 1e-10)
+        self.prox = CGClass(A, AH, max_iter=max_iter, tol=tol)
+
+    @property
+    def weight(self):
+        return self._weight * self.weight_scale
+
+    def forward(self, inputs, scale=1.0):
+        x = inputs[0]
+        y = inputs[1]
+        constants = inputs[2:]
+        lambdaa = 1.0 / torch.max(self.weight * scale, torch.ones_like(self.weight)*1e-9)
+        return self.prox(lambdaa, x, y, *constants)
+
+class CgTest(unittest.TestCase):
+    def testcg(self):
+        from merlinth.layers.mri import MulticoilForwardOp, MulticoilAdjointOp
+        A = MulticoilForwardOp().double()
+        AH = MulticoilAdjointOp().double()
+
+        dc = DCPM(A, AH, weight_init=1.0, max_iter=100, tol=1e-18)
+
+        shape=(5,1,10,10)
+        kshape=(5,3,10,10)
+        x = merlinth.random_normal_complex(shape, torch.float64)
+        y = merlinth.random_normal_complex(kshape, torch.float64)
+        mask = torch.ones(*kshape, dtype=torch.float64)
+        smaps = merlinth.random_normal_complex(kshape, torch.float64)
+
+        th_a = torch.tensor(1.1, requires_grad=True, dtype=torch.float64)
+        th_b = torch.tensor(1.1, requires_grad=True, dtype=torch.float64)
+
+        # perform a gradient check:
+        epsilon = 1e-5
+
+        def compute_loss(a, b):
+            arg = dc([x*a, y, mask, smaps], scale=1/b) # take 1/b
+            return 0.5 * torch.sum(torch.real(torch.conj(arg) * arg))
+
+        th_loss = compute_loss(th_a, th_b)
+
+        # backpropagate the gradient
+        th_loss.backward()
+        grad_a = th_a.grad.cpu().numpy()
+        grad_b = th_b.grad.cpu().numpy()
+
+        # numerical gradient w.r.t. the input
+        with torch.no_grad():
+            l_ap = compute_loss(th_a+epsilon, th_b).cpu().numpy()
+            l_an = compute_loss(th_a-epsilon, th_b).cpu().numpy()
+            grad_a_num = (l_ap - l_an) / (2 * epsilon)
+
+        print("grad_x: {:.7f} num_grad_x {:.7f} success: {}".format(
+            grad_a, grad_a_num, np.abs(grad_a - grad_a_num) < 1e-4))
+        self.assertTrue(np.abs(grad_a - grad_a_num) < 1e-4)
+
+        # numerical gradient w.r.t. the weights
+        with torch.no_grad():
+            l_bp = compute_loss(th_a, th_b+epsilon).cpu().numpy()
+            l_bn = compute_loss(th_a, th_b-epsilon).cpu().numpy()
+            grad_b_num = (l_bp - l_bn) / (2 * epsilon)
+
+        print("grad_w: {:.7f} num_grad_w {:.7f} success: {}".format(
+            grad_b, grad_b_num, np.abs(grad_b - grad_b_num) < 1e-4))
+        self.assertTrue(np.abs(grad_b - grad_b_num) < 1e-4)
+        
+if __name__ == "__main__":
+    unittest.test()
---
+++ pytorch/merlinth/layers/mri.py
@@ -0,0 +1,124 @@
+import torch
+from merlinth.layers.fft import fft2, fft2c, ifft2, ifft2c
+import unittest
+import merlinth
+
+class MulticoilForwardOp(torch.nn.Module):
+    def __init__(self, center=False, coil_axis=-3, channel_dim_defined=True):
+        super().__init__()
+        if center:
+            self.fft2 = fft2c
+        else:
+            self.fft2 = fft2
+        self.coil_axis = coil_axis
+        self.channel_dim_defined = channel_dim_defined
+
+    def forward(self, image, mask, smaps):
+        if self.channel_dim_defined:
+            coilimg = torch.unsqueeze(image[:,0], self.coil_axis) * smaps
+        else:
+            coilimg = torch.unsqueeze(image, self.coil_axis) * smaps
+        kspace = self.fft2(coilimg)
+        masked_kspace = kspace * mask
+        return masked_kspace
+
+class MulticoilAdjointOp(torch.nn.Module):
+    def __init__(self, center=False, coil_axis=-3, channel_dim_defined=True):
+        super().__init__()
+        if center:
+            self.ifft2 = ifft2c
+        else:
+            self.ifft2 = ifft2
+        self.coil_axis = coil_axis
+        self.channel_dim_defined = channel_dim_defined
+
+    def forward(self, kspace, mask, smaps):
+        masked_kspace = kspace * mask
+        coilimg = self.ifft2(masked_kspace)
+        img = torch.sum(torch.conj(smaps) * coilimg, self.coil_axis)
+
+        if self.channel_dim_defined:
+            return torch.unsqueeze(img, 1)
+        else:
+            return img
+
+class ForwardOp(torch.nn.Module):
+    def __init__(self, center=False):
+        super().__init__()
+        if center:
+            self.fft2 = fft2c
+        else:
+            self.fft2 = fft2
+
+    def forward(self, image, mask):
+        kspace = self.fft2(image)
+        masked_kspace = kspace * mask
+        return masked_kspace
+
+class AdjointOp(torch.nn.Module):
+    def __init__(self, center=False):
+        super().__init__()
+        if center:
+            self.ifft2 = ifft2c
+        else:
+            self.ifft2 = ifft2
+
+    def forward(self, kspace, mask):
+        masked_kspace = kspace * mask
+        img = self.ifft2(masked_kspace)
+        return img
+
+class TestMulticoilOps(unittest.TestCase):        
+    def _get_data(self, shape, channel_dim_defined, dtype=torch.get_default_dtype()):
+        batch, frames, ncoils, M, N = shape
+        img = merlinth.random_normal_complex((batch, frames, M, N), dtype)
+        mask = torch.ones((batch, 1, frames, 1, N), dtype=dtype)
+        smaps = merlinth.random_normal_complex((batch, ncoils, 1, M, N), dtype)
+        kspace = merlinth.random_normal_complex((batch, ncoils, frames, M, N), dtype)
+        if channel_dim_defined:
+            img = img[:,None]
+        return img, mask, smaps, kspace
+
+    def _test_forward(self, channel_dim_defined):
+        shape = (2, 10, 15, 100, 100)
+        img, mask, smaps, kspace = self._get_data(shape, channel_dim_defined)
+        A = MulticoilForwardOp(center=True, coil_axis=1, channel_dim_defined=channel_dim_defined)
+        Ax = A(img, mask, smaps)
+        self.assertTrue(Ax.shape == kspace.shape)
+
+    def test_forward(self):
+        self._test_forward(False)
+
+    def test_forward_channel_dim(self):
+        self._test_forward(True)
+
+    def _test_backward(self, channel_dim_defined):
+        shape = (2, 10, 15, 100, 100)
+        img, mask, smaps, kspace = self._get_data(shape, channel_dim_defined)
+        AH = MulticoilAdjointOp(center=True, coil_axis=1, channel_dim_defined=channel_dim_defined)
+        AHy = AH(kspace, mask, smaps)
+        self.assertTrue(AHy.shape == img.shape)
+
+    def test_backward(self):
+        self._test_backward(False)
+
+    def test_backward_channel_dim(self):
+        self._test_backward(True)
+
+    def _test_adjointness(self, channel_dim_defined):
+        shape = (2, 10, 15, 100, 100)
+        img, mask, smaps, kspace = self._get_data(shape, channel_dim_defined, torch.double)
+        A = MulticoilForwardOp(center=True, coil_axis=1, channel_dim_defined=channel_dim_defined).double()
+        AH = MulticoilAdjointOp(center=True, coil_axis=1, channel_dim_defined=channel_dim_defined).double()
+        Ax = A(img, mask, smaps,)
+        AHy = AH(kspace, mask, smaps)
+
+        lhs = torch.sum(Ax * torch.conj(kspace)).numpy()
+        rhs = torch.sum(img * torch.conj(AHy)).numpy()
+        self.assertAlmostEqual(lhs, rhs)
+
+    def test_adjointness(self):
+        self._test_adjointness(False)
+
+    def test_adjointness_channel_dim(self):
+        self._test_adjointness(True)
