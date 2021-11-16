6f664abac987736c2378f49975a5fafaf97a13cc
Kerstin Hammernik <k.hammernik@tum.de>
2021-11-16 17:41:55 +0100

    #49 add motion mri ops.

--- pytorch/merlinth/layers/mri.py
+++ pytorch/merlinth/layers/mri.py
@@ -1,5 +1,6 @@
 import torch
 from merlinth.layers.fft import fft2, fft2c, ifft2, ifft2c
+from merlinth.layers.warp import WarpForward, WarpAdjoint
 import unittest
 import merlinth
 
@@ -77,6 +78,37 @@ class ForwardOp(torch.nn.Module):
         masked_kspace = kspace * mask
         return masked_kspace
 
+
+class MulticoilMotionForwardOp(torch.nn.Module):
+    def __init__(self, center=False, coil_axis=-3, channel_dim_defined=True):
+        super().__init__()
+        self.W = WarpForward()
+        self.A = MulticoilForwardOp(center=center, coil_axis=coil_axis, channel_dim_defined=False)
+        self.channel_dim_defined = channel_dim_defined
+
+    def forward(self, x, mask, smaps, u):
+        if self.channel_dim_defined:
+            x = self.W(x[:,0], u)
+        else:
+            x = self.W(x, u)
+        y = self.A(x, mask, smaps)
+        return y
+
+class MulticoilMotionAdjointOp(torch.nn.Module):
+    def __init__(self, center=False, coil_axis=-3, channel_dim_defined=True):
+        super().__init__()
+        self.AH = MulticoilAdjointOp(center=center, coil_axis=coil_axis, channel_dim_defined=False)
+        self.WH = WarpAdjoint()
+        self.channel_dim_defined = channel_dim_defined
+
+    def forward(self, y, mask, smaps, u):
+        x = self.AH(y, mask, smaps)
+        x = self.WH(x, u)
+        if self.channel_dim_defined:
+            return torch.unsqueeze(x, 1)
+        else:
+            return x
+
 class AdjointOp(torch.nn.Module):
     def __init__(self, center=False):
         super().__init__()
@@ -138,6 +170,67 @@ class TestMulticoilOps(unittest.TestCase):
         lhs = torch.sum(Ax * torch.conj(kspace)).numpy()
         rhs = torch.sum(img * torch.conj(AHy)).numpy()
         self.assertAlmostEqual(lhs, rhs)
+
+    def test_adjointness(self):
+        self._test_adjointness(False)
+
+    def test_adjointness_channel_dim(self):
+        self._test_adjointness(True)
+
+
+class TestMulticoilMotionOps(unittest.TestCase):
+    #TODO test warping if features are present
+    def _get_data(self, shape, channel_dim_defined, dtype=torch.double):
+        batch, frames, frames_all, M, N, ncoils = shape
+        img = merlinth.random_normal_complex((batch, frames, M, N), dtype=dtype)
+        uv = torch.randn((batch, frames, frames_all, M, N, 2), dtype=dtype)
+        mask = torch.ones((batch, 1, 1, frames_all, 1, N), dtype=dtype)
+        smaps = merlinth.random_normal_complex((batch, ncoils, 1, 1, M, N), dtype=dtype)
+        kspace = merlinth.random_normal_complex((batch, ncoils, frames, frames_all, M, N), dtype=dtype)
+        if channel_dim_defined:
+            img = img[:,None]
+        return img.cuda(), uv.cuda(), mask.cuda(), smaps.cuda(), kspace.cuda()
+
+    def _test_forward(self, channel_dim_defined):
+        shape = (2, 4, 25, 176, 132, 15)
+        img, uv, mask, smaps, kspace = self._get_data(shape, channel_dim_defined)
+
+        A = MulticoilMotionForwardOp(center=True, coil_axis=1, channel_dim_defined=channel_dim_defined).cuda()
+        Ax = A(img, mask, smaps, uv)
+        self.assertTrue(Ax.shape == kspace.shape)
+
+    def test_forward(self):
+        self._test_forward(False)
+
+    def test_forward_channel_dim(self):
+        self._test_forward(True)
+    
+    def _test_backward(self, channel_dim_defined):
+        shape = (2, 4, 25, 176, 132, 15)
+        img, uv, mask, smaps, kspace = self._get_data(shape, channel_dim_defined)
+
+        AH = MulticoilMotionAdjointOp(center=True, coil_axis=1, channel_dim_defined=channel_dim_defined).cuda()
+        AHy = AH(kspace, mask, smaps, uv)
+        self.assertTrue(AHy.shape == img.shape)
+
+    def test_backward(self):
+        self._test_backward(False)
+
+    def test_backward_channel_dim(self):
+        self._test_backward(True)
+
+    def _test_adjointness(self, channel_dim_defined):
+        shape = (2, 4, 25, 176, 132, 15)
+        img, uv, mask, smaps, kspace = self._get_data(shape, channel_dim_defined)
+
+        A = MulticoilMotionForwardOp(center=True, coil_axis=1, channel_dim_defined=channel_dim_defined).cuda()
+        AH = MulticoilMotionAdjointOp(center=True, coil_axis=1, channel_dim_defined=channel_dim_defined).cuda()
+        Ax = A(img, mask, smaps, uv)
+        AHy = AH(kspace, mask, smaps, uv)
+
+        lhs = torch.sum(Ax * torch.conj(kspace))
+        rhs = torch.sum(img * torch.conj(AHy))
+        self.assertAlmostEqual(lhs.cpu().numpy(), rhs.cpu().numpy())
 
     def test_adjointness(self):
         self._test_adjointness(False)
