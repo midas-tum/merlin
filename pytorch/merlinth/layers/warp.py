4edec10498b8a7e9fdf6f67e5205551c715077d3
Kerstin Hammernik <k.hammernik@tum.de>
2021-12-07 21:59:33 +0100

    #49 adapt code due to changes in optox.

--- pytorch/merlinth/layers/warp.py
+++ pytorch/merlinth/layers/warp.py
@@ -14,12 +14,7 @@ class WarpForward(torch.nn.Module):
         x = torch.repeat_interleave(torch.unsqueeze(x, -3), repeats=u.shape[-4], dim=-3)
         x = torch.reshape(x, (-1, 1, M, N)) # [batch, frames * frames_all, 1, M, N]
         u = torch.reshape(u, (-1, M, N, 2)) # [batch, frames * frames_all, M, N, 2]
-        if x.is_complex():
-            Wx_re = optoth.warp.WarpFunction().apply(torch.real(x).contiguous(), u)
-            Wx_im = optoth.warp.WarpFunction().apply(torch.imag(x).contiguous(), u)
-            Wx = torch.complex(Wx_re, Wx_im)
-        else:
-            Wx = optoth.warp.WarpFunction().apply(x, u)
+        Wx = optoth.warp.WarpFunction.apply(x, u)
         return torch.reshape(Wx, out_shape)
 
 class WarpAdjoint(torch.nn.Module):
@@ -31,26 +26,17 @@ class WarpAdjoint(torch.nn.Module):
         M, N = u.shape[-3:-1]
         x = torch.reshape(x, (-1, 1, M, N)) # [batch * frames * frames_all, 1, M, N]
         u = torch.reshape(u, (-1, M, N, 2)) # [batch * frames * frames_all, M, N, 2]
-        if x.is_complex():
-            x_warpT_re = optoth.warp.WarpTransposeFunction().apply(torch.real(x).contiguous(), u)
-            x_warpT_im = optoth.warp.WarpTransposeFunction().apply(torch.imag(x).contiguous(), u)
-            x_warpT = torch.complex(x_warpT_re, x_warpT_im)
-        else:
-            x_warpT = optoth.warp.WarpTransposeFunction().apply(x, u)
+        x_warpT = optoth.warp.WarpTransposeFunction.apply(x, u)
         x_warpT = torch.reshape(x_warpT, out_shape)
         x_warpT = torch.sum(x_warpT, -3)
         return x_warpT
-
     
 class TestWarping(unittest.TestCase):
     def _get_data(self, shape, is_complex):
         batch, frames, frames_all, M, N = shape
-        if is_complex:
-            img = merlinth.random_normal_complex((batch, frames, M, N), dtype=torch.double)
-            imgT = merlinth.random_normal_complex((batch, frames, frames_all, M, N), dtype=torch.double)
-        else:
-            img = torch.randn((batch, frames, M, N), dtype=torch.double)
-            imgT = torch.randn((batch, frames, frames_all, M, N), dtype=torch.double)
+        cdtype = torch.complex128 if is_complex else torch.double
+        img = torch.randn((batch, frames, M, N), dtype=cdtype)
+        imgT = torch.randn((batch, frames, frames_all, M, N), dtype=cdtype)
         uv = torch.randn((batch, frames, frames_all, M, N, 2), dtype=torch.double)
         return img.cuda(), imgT.cuda(), uv.cuda()
 
@@ -101,15 +87,12 @@ class TestWarping(unittest.TestCase):
 
     def _test_forward_grad(self, is_complex):
         shape = (2, 4, 25, 176, 132)
-        img, imgT, uv = self._get_data(shape, is_complex)
+        img, _, uv = self._get_data(shape, is_complex)
         W = WarpForward().cuda()
         img.requires_grad_(True)
         uv.requires_grad_(True)
         Wx = W(img, uv)
-        if is_complex:
-            loss = torch.sum(torch.real(torch.conj(Wx) * Wx))
-        else:
-            loss = torch.sum(Wx * Wx)
+        loss = torch.norm(Wx)**2
         loss.backward()
 
         self.assertTrue(img.grad != None)
@@ -123,15 +106,12 @@ class TestWarping(unittest.TestCase):
 
     def _test_backward_grad(self, is_complex):
         shape = (2, 4, 25, 176, 132)
-        img, imgT, uv = self._get_data(shape, is_complex)
+        _, imgT, uv = self._get_data(shape, is_complex)
         WH = WarpAdjoint().cuda()
         imgT.requires_grad_(True)
         uv.requires_grad_(True)
         WHx = WH(imgT, uv)
-        if is_complex:
-            loss = torch.sum(torch.real(torch.conj(WHx) * WHx))
-        else:
-            loss = torch.sum(WHx * WHx)
+        loss = torch.norm(WHx)**2
         loss.backward()
 
         self.assertTrue(imgT.grad != None)
