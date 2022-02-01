e64001926b1a9e31d2e6374b66be8857e5708a20
Thomas <thomas.kuestner@uni-tuebingen.de>
2022-02-01 14:37:57 +0100

    #43 add pooling interfaces Torch

--- pytorch/merlinth/layers/complex_avgpool.py
+++ pytorch/merlinth/layers/complex_avgpool.py
@@ -1,17 +1,17 @@
 import torch
 import merlinth
 try:
-    import optoth.maxpooling
+    import optoth.averagepooling
 except:
     print('optoth could not be imported')
 
 def get(identifier):
-    return MagnitudeMaxPooling(identifier)
+    return MagnitudeAveragePooling(identifier)
 
 
-def MagnitudeMaxPooling(identifier):
+def MagnitudeAveragePooling(identifier):
     if isinstance(identifier, six.string_types):
-        identifier = 'MagnitudeMaxPool' + str(identifier).upper().replace('T', 't')
+        identifier = 'MagnitudeAveragePool' + str(identifier).upper().replace('T', 't')
         return deserialize(identifier)
     elif callable(identifier):
         return identifier
@@ -20,24 +20,24 @@ def MagnitudeMaxPooling(identifier):
 
 
 def deserialize(op):
-    if op == 'MagnitudeMaxPool1D' or op == 'MagnitudeMaxPooling1D':
-        return MagnitudeMaxPool1D
-    elif op == 'MagnitudeMaxPool2D' or op == 'MagnitudeMaxPooling2D':
-        return MagnitudeMaxPool2D
-    elif op == 'MagnitudeMaxPool2Dt' or op == 'MagnitudeMaxPooling2Dt':
-        return MagnitudeMaxPool2Dt
-    elif op == 'MagnitudeMaxPool3D' or op == 'MagnitudeMaxPooling3D':
-        return MagnitudeMaxPool3D
-    elif op == 'MagnitudeMaxPool3Dt' or op == 'MagnitudeMaxPooling3Dt':
-        return MagnitudeMaxPool3Dt
+    if op == 'MagnitudeAveragePool1D' or op == 'MagnitudeAveragePooling1D':
+        return MagnitudeAveragePool1D
+    elif op == 'MagnitudeAveragePool2D' or op == 'MagnitudeAveragePooling2D':
+        return MagnitudeAveragePool2D
+    elif op == 'MagnitudeAveragePool2Dt' or op == 'MagnitudeAveragePooling2Dt':
+        return MagnitudeAveragePool2Dt
+    elif op == 'MagnitudeAveragePool3D' or op == 'MagnitudeAveragePooling3D':
+        return MagnitudeAveragePool3D
+    elif op == 'MagnitudeAveragePool3Dt' or op == 'MagnitudeAveragePooling3Dt':
+        return MagnitudeAveragePool3Dt
     else:
         raise ValueError(f"Selected operation '{op}' not implemented in complex convolutional")
 
 
 
-class MagnitudeMaxPool(torch.nn.Module):
+class MagnitudeAveragePool(torch.nn.Module):
     def __init__(self, kernel_size, stride=None, padding=0, dilation=1, padding_mode='SAME', return_indices=False, rank=2):
-        super(MagnitudeMaxPool, self).__init__()
+        super(MagnitudeAveragePool, self).__init__()
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
@@ -47,13 +47,13 @@ class MagnitudeMaxPool(torch.nn.Module):
         self.padding_mode = padding_mode
         self.return_indices = return_indices
         self.channel_first = True  # default PyTorch order: [N, C, H, W, ....]
-        self.optox = optox and (True if 'optoth.maxpooling' in sys.modules else False)
+        self.optox = optox and (True if 'optoth.averagepooling' in sys.modules else False)
 
         if not self.optox:
             if rank == 3:
-                self.pool = torch.nn.MaxPool3d(kernel_size, stride, return_indices=True)
+                self.pool = torch.nn.AvgPool3d(kernel_size, stride, return_indices=True)
             elif rank == 2:
-                self.pool = torch.nn.MaxPool2d(kernel_size, stride, return_indices=True)
+                self.pool = torch.nn.AvgPool2d(kernel_size, stride, return_indices=True)
             else:
                 raise ValueError(f"pooling for dim={rank} not defined")
     
@@ -70,59 +70,59 @@ class MagnitudeMaxPool(torch.nn.Module):
         return output
 
 
-class MagnitudeMaxPool2D(MagnitudeMaxPool):
+class MagnitudeAveragePool2D(MagnitudeAveragePool):
     def __init__(self, kernel_size, stride=(2, 2), padding=(0, 0), dilation=(1, 1), padding_mode='SAME', return_indices=False):
         super().__init__(kernel_size, stride, padding, dilation, return_indices, rank=2)
 
     def forward(self, x):
-        return optoth.maxpooling.maxpooling2d(x, self.kernel_size, self.padding, self.stride, self.dilation,
+        return optoth.averagepooling.averagepooling2d(x, self.kernel_size, self.padding, self.stride, self.dilation,
                                               self.alpha, self.beta, self.padding_mode, x.dtype, self.channel_first)
 
     def backward(self, grad_output):
-        return optoth.maxpooling.maxpooling2d_backward(TODO, TODO, TODO, self.kernel_size, self.padding, self.stride, self.dilation,
+        return optoth.averagepooling.averagepooling2d_backward(TODO, TODO, TODO, self.kernel_size, self.padding, self.stride, self.dilation,
                                                        self.alpha, self.beta, self.padding_mode, x.dtype, self.channel_first)
 
-class MagnitudeMaxPool3D(MagnitudeMaxPool):
+class MagnitudeAveragePool3D(MagnitudeAveragePool):
     def __init__(self, kernel_size, stride=(2, 2, 2), padding=(0, 0, 0), dilation=(1, 1, 1), padding_mode='SAME', return_indices=False):
         super().__init__(kernel_size, stride, padding, dilation, return_indices, rank=3)
 
     def forward(self, x):
-        return optoth.maxpooling.maxpooling3d(x, self.kernel_size, self.padding, self.stride, self.dilation,
+        return optoth.averagepooling.averagepooling3d(x, self.kernel_size, self.padding, self.stride, self.dilation,
                                               self.alpha, self.beta, self.padding_mode, x.dtype, self.channel_first)
 
     def backward(self, grad_output):
-        return optoth.maxpooling.maxpooling3d_backward(TODO, TODO, TODO, self.kernel_size, self.padding, self.stride,
+        return optoth.averagepooling.averagepooling3d_backward(TODO, TODO, TODO, self.kernel_size, self.padding, self.stride,
                                                        self.dilation,
                                                        self.alpha, self.beta, self.padding_mode, x.dtype,
                                                        self.channel_first)
 
-class MagnitudeMaxPool2Dt(MagnitudeMaxPool):
+class MagnitudeAveragePool2Dt(MagnitudeAveragePool):
     def __init__(self, kernel_size, stride=(2, 2, 2), padding=(0, 0, 0), dilation=(1, 1, 1), padding_mode='SAME', return_indices=False):
         super().__init__(kernel_size, stride, padding, dilation, return_indices, rank=3)
 
     def forward(self, x):
-        return optoth.maxpooling.maxpooling3d(x, self.kernel_size, self.padding, self.stride, self.dilation,
+        return optoth.averagepooling.averagepooling3d(x, self.kernel_size, self.padding, self.stride, self.dilation,
                                               self.alpha, self.beta, self.padding_mode, x.dtype, self.channel_first)
 
     def backward(self, grad_output):
-        return optoth.maxpooling.maxpooling3d_backward(TODO, TODO, TODO, self.kernel_size, self.padding, self.stride,
+        return optoth.averagepooling.averagepooling3d_backward(TODO, TODO, TODO, self.kernel_size, self.padding, self.stride,
                                                        self.dilation,
                                                        self.alpha, self.beta, self.padding_mode, x.dtype,
                                                        self.channel_first)
 
-class MagnitudeMaxPool3Dt(MagnitudeMaxPool):
+class MagnitudeAveragePool3Dt(MagnitudeAveragePool):
     def __init__(self, kernel_size, stride=(2, 2, 2, 2), padding=(0, 0, 0, 0), dilation=(1, 1, 1, 1), padding_mode='SAME', return_indices=False):
         super().__init__(kernel_size, stride, padding, dilation, return_indices, rank=4)
 
     def forward(self, x):
-        return optoth.maxpooling.maxpooling4d(x, self.kernel_size, self.padding, self.stride, self.dilation,
+        return optoth.averagepooling.averagepooling4d(x, self.kernel_size, self.padding, self.stride, self.dilation,
                                               self.alpha, self.beta, self.padding_mode, x.dtype, self.channel_first)
 
     def backward(self, grad_output):
-        return optoth.maxpooling.maxpooling4d_backward(TODO, TODO, TODO, self.kernel_size, self.padding, self.stride,
+        return optoth.averagepooling.averagepooling4d_backward(TODO, TODO, TODO, self.kernel_size, self.padding, self.stride,
                                                        self.dilation,
                                                        self.alpha, self.beta, self.padding_mode, x.dtype,
                                                        self.channel_first)
 
 # Aliases
-MagnitudeMaxPool4D = MagnitudeMaxPool3Dt
+MagnitudeAveragePool4D = MagnitudeAveragePool3Dt
