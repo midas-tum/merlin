2fdd107ec5a06d3445b8509b46bf874180be870e
Thomas <thomas.kuestner@uni-tuebingen.de>
2022-05-02 20:29:37 +0200

    clean up for pub

--- pytorch/merlinth/layers/__init__.py
+++ pytorch/merlinth/layers/__init__.py
@@ -55,7 +55,6 @@ from merlinth.layers.fft import ifft2c
 # from .complex_foe2d import *
 # from .complex_foe3d import *
 # from .complex_layer import *
-# #from .complex_tdv import *
 # from .unet import *
 # from .complex_act import *
 # from .foe2d import *
