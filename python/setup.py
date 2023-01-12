4c7b6c91c77ccf0e8ebdf96794923960ed56988c
Thomas <thomas.kuestner@uni-tuebingen.de>
2023-01-12 17:13:31 +0100

    create python-ver-independent build

--- python/setup.py
+++ python/setup.py
@@ -8,21 +8,22 @@ import subprocess
 import io
 import importlib.util
 
-class BinaryDistribution(Distribution):
-    def has_ext_modules(self):
-        return True
-
 # Package meta-data
 DESCRIPTION = 'Machine Enhanced Reconstruction Learning and Interpretation Networks (MERLIN) - merlinpy'
-VERSION = '0.3.2'
+VERSION = '0.3.8'
 
 currdir = os.path.abspath(os.path.dirname(__file__))  #os.getcwd()
 compilepath = os.path.join('merlinpy', 'datapipeline', 'sampling', 'PoissonDisc')
-os.chdir(compilepath)
-subprocess.run(['python', 'setup_VDPD.py', 'build'])
-os.chdir(currdir)
-libpath = [p for p in os.listdir(os.path.join(compilepath, 'build')) if p.startswith('lib')][0]
-shutil.copyfile(os.path.join(compilepath, 'build', libpath, os.listdir(os.path.join(compilepath, 'build', libpath))[0]), os.path.join('merlinpy', 'datapipeline', 'sampling', 'VDPDGauss.so'))
+
+if len(sys.argv) > 1 and sys.argv[1] == 'sampling':
+    INCLUDE_PACKAGE_DATA = True
+    PACKAGE_DATA = {"merlinpy.datapipeline.sampling": ['VDPDGauss.so'],}
+else:
+    INCLUDE_PACKAGE_DATA = False
+    PACKAGE_DATA = {}
+# check if run via pip install or locally (enable build of sampling code)
+# pip requires manylinux conform code, hence C++ sampling code need to be compiled for each GLIBC, Python and header versions
+# -> opting for local workaround: build the sampling code yourself locally using "python3 setup.py
 
 # Readme
 try:
@@ -31,10 +32,17 @@ try:
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-class UploadCommand(Command):
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        if INCLUDE_PACKAGE_DATA:
+            return True
+        else:
+            return False
+
+class SamplingCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = 'Build and install the package.'
     user_options = []
 
     @staticmethod
@@ -49,9 +57,54 @@ class UploadCommand(Command):
         pass
 
     def run(self):
+        self.status('Build and install locally - for sampling code')
         try:
             self.status('Removing previous builds…')
             shutil.rmtree(os.path.join(currdir, 'dist'))
+            shutil.rmtree(os.path.join(currdir, 'build'))
+            shutil.rmtree(os.path.join(currdir, 'wheelhouse'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.chdir(compilepath)
+        subprocess.run(['python3', 'setup_VDPD.py', 'build'])
+        os.chdir(currdir)
+        libpath = [p for p in os.listdir(os.path.join(compilepath, 'build')) if p.startswith('lib')][0]
+        shutil.copyfile(
+            os.path.join(compilepath, 'build', libpath, os.listdir(os.path.join(compilepath, 'build', libpath))[0]),
+            os.path.join('merlinpy', 'datapipeline', 'sampling', 'VDPDGauss.so'))
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Installing locally')
+        wheelfile = [f for f in os.listdir(os.path.join(currdir, 'dist')) if f.endswith('.whl')]
+        os.system('pip install ' + os.path.join(currdir, 'dist', wheelfile[0]))
+
+        sys.exit()
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and upload the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.status('Build and upload wheel')
+        try:
+            self.status('Removing previous builds…')
+            shutil.rmtree(os.path.join(currdir, 'dist'))
+            shutil.rmtree(os.path.join(currdir, 'build'))
             shutil.rmtree(os.path.join(currdir, 'wheelhouse'))
         except OSError:
             pass
@@ -59,6 +112,7 @@ class UploadCommand(Command):
         self.status('Building Source and Wheel (universal) distribution…')
         os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
 
+        '''
         self.status('Conform the Wheel to manylinux2010')
         namemodule = 'auditwheel'
         spec = importlib.util.find_spec(namemodule)
@@ -74,9 +128,10 @@ class UploadCommand(Command):
             print(f"Installing the {namemodule!r} module")
             os.system('pip install ' + namemodule)
         os.system('auditwheel repair dist/merlinpy-' + VERSION + '-cp38-cp38-linux_x86_64.whl')
+        '''
 
         self.status('Uploading the package to PyPI via Twine…')
-        os.system('python -m twine upload --repository testpypi wheelhouse/*')
+        os.system('python -m twine upload --repository testpypi dist/*')
 
         self.status('Pushing git tags…')
         os.chdir(os.path.dirname(currdir))
@@ -110,9 +165,8 @@ setup(
                  "merlinpy.datapipeline.sampling.VISTA": os.path.join('.', "merlinpy/datapipeline/sampling/VISTA"),
                  "merlinpy.test": os.path.join('.', "merlinpy/test"),
     },
-    include_package_data=True,
-    package_data={"merlinpy.datapipeline.sampling": ['VDPDGauss.so'],
-    },
+    include_package_data=INCLUDE_PACKAGE_DATA,
+    package_data=PACKAGE_DATA,
     install_requires=[
         "numpy >= 1.15",
         "xmltodict",
@@ -144,5 +198,6 @@ setup(
     ],
     cmdclass={
         'upload': UploadCommand,
+        'sampling': SamplingCommand
     }
 )
